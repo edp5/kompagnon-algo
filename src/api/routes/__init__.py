4879902ed5b84b47e3f7843721f5c2891726ca4f
Adrien Le Guen <adri1.leguen@gmail.com>
2026-06-04 10:21:09 +0200

    delete routes in init.py

--- src/api/routes/__init__.py
+++ src/api/routes/__init__.py
@@ -2,8 +2,6 @@ from fastapi import APIRouter
 
 from src.api.routes.root import router as root_router
 from src.api.routes.status import router as status_router
-from src.api.routes.get_valid import router as get_valid_router
-from src.api.routes.get_invalid import router as get_invalid_router
 from src.api.routes.put_journey import router as put_journey_router
 from src.api.routes.match import router as match_router
 
@@ -12,7 +10,5 @@ router = APIRouter()
 # Attach each modular route to the main API router
 router.include_router(root_router)
 router.include_router(status_router)
-router.include_router(get_valid_router)
-router.include_router(get_invalid_router)
 router.include_router(put_journey_router)
 router.include_router(match_router)
