cb2c5e31c61a949f15053856c4730b04599eed54
Adrien Le Guen <adri1.leguen@gmail.com>
2026-06-04 10:29:03 +0200

    delete put-journey

--- src/api/routes/__init__.py
+++ src/api/routes/__init__.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter
 
 from src.api.routes.root import router as root_router
 from src.api.routes.status import router as status_router
-from src.api.routes.put_journey import router as put_journey_router
 from src.api.routes.match import router as match_router
 
 router = APIRouter()
@@ -10,5 +9,4 @@ router = APIRouter()
 # Attach each modular route to the main API router
 router.include_router(root_router)
 router.include_router(status_router)
-router.include_router(put_journey_router)
 router.include_router(match_router)
